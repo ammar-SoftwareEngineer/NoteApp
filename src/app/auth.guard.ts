3f8cb37014776f4b14e14b9216f6ca24bd2447e4
ammar yasser <aboamer0548@gmail.com>
2024-03-17 15:56:28 +0200

    update

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,11 +2,11 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  let _router = inject(Router);
+  let _Router = inject(Router);
   if (localStorage.getItem('token') !== null) {
     return true;
   } else {
-    _router.navigate(['login']);
+    _Router.navigate(['/login']);
     return false;
   }
 };
